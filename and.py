3c14d3b59c27273ec4c1d2800137cd1930204126
Arpan Ghosh <argh53@gmail.com>
2021-09-16 20:24:57 +0530

    modeulerazied and or xor.py files

--- and.py
+++ and.py
@@ -4,30 +4,33 @@ from utils.all_utils import prepare_data
 import pandas as pd
 import numpy as np
 
+def main(data, eta, epochs, filename, plotfilename):
 
-AND = {
+
+    df = pd.DataFrame(data)
+
+    X,y = prepare_data(df)
+
+
+
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    save_model(model, filename=filename)
+    save_plot(df, plotfilename, model)
+
+if __name__=='__main__': #starting point
+
+    
+    AND = {
     "x1": [0,0,1,1],
     "x2": [0,1,0,1],
     "y": [0,0,0,1],
-}
+    }
+    
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
 
-df = pd.DataFrame(AND)
-
-df
-x1	x2	y
-0	0	0	0
-1	0	1	0
-2	1	0	0
-3	1	1	1
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-
-save_model(model, filename="and.model")
-save_plot(df, "and.png", model)
+    main(data=AND, eta=ETA, epochs=EPOCHS, filename="and.model", plotfilename="and.model")
--- xor.py
+++ xor.py
@@ -1,28 +1,38 @@
+from Perceptron.utils.all_utils import save_model, save_plot
 from utils.model import Perceptron
 from utils.all_utils import prepare_data
 import pandas as pd
 import numpy as np
 
-XOR = {
+
+
+def main(data, eta, epochs, filename, plotfilename):
+
+
+    df = pd.DataFrame(data)
+
+    X,y = prepare_data(df)
+
+
+
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    save_model(model, filename=filename)
+    save_plot(df, plotfilename, model)
+
+if __name__=='__main__': #starting point
+
+    
+    XOR = {
     "x1": [0,0,1,1],
     "x2": [0,1,0,1],
     "y": [0,1,1,0],
-}
+    }
+    
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
 
-df = pd.DataFrame(XOR)
-
-df
-x1	x2	y
-0	0	0	0
-1	0	1	1
-2	1	0	1
-3	1	1	0
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model_XOR = Perceptron(eta=ETA, epochs=EPOCHS)
-model_XOR.fit(X, y)
-
-_ = model_XOR.total_loss()
+    main(data=XOR, eta=ETA, epochs=EPOCHS, filename="xor.model", plotfilename="xor.model")
